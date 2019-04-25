f2a8854f4644706c73d70d72acb84934e28aee26
Miller <sara.miller@dfg.alaska.local>
2019-04-25 10:09:26 -0800

    clean up code for clarity

--- code/functions.R
+++ code/functions.R
@@ -1,4 +1,3 @@
-
 BTSPAS_input <- function(recovery, commercial, 
                          rel.stratum, rec.stratum, 
                          stratum.index, catch.var="CdnCommCt"){
@@ -9,109 +8,106 @@ BTSPAS_input <- function(recovery, commercial,
   if(length(commercial[,rec.stratum])==0){
     stop("Recovery strata variables don't exists in commercial data frame")
   }  
-  #browser()
-  # only want recoveries from commercial fishery. If the RecoveryType is anything else
-  # then convert the rec.stratum to missing
+
+# only want recoveries from commercial fishery. If the RecoveryType is anything else
+# then convert the rec.stratum to missing
   recovery[,rec.stratum][!recovery$RecoveryType %in% "Commercial"] <- NA
   
-  # convert release stratum to character form and check for missing values
-  # if the stratum has an "NA" in the stratum value, it is also considered to be missing
+# convert release stratum to character form and check for missing values
+# if the stratum has an "NA" in the stratum value, it is also considered to be missing
   recovery[, rel.stratum] <- as.character(recovery[, rel.stratum])
-  # Check that no missing values in release statum
+# Check that no missing values in release statum
   if(sum(grepl("NA",recovery[,rel.stratum]) | is.na(recovery[,rel.stratum]))){
     stop("Missing values not allowed in release stratum")
   }
-  # Check for no missing values in commercial recovery strata
+# Check for no missing values in commercial recovery strata
   if(sum(grepl("NA",commercial[,rec.stratum]) | is.na(commercial[,rec.stratum]))){
     stop("Missing values not allowed in commercial recoveries")
   }
   
-  #browser()
-  # convert release stratum to character form and set to missing any stratum value that contains NA 
+# convert release stratum to character form and set to missing any stratum value that contains NA 
   recovery[, rec.stratum] <- as.character(recovery[, rec.stratum])
   recovery[, rec.stratum][ grepl("NA", recovery[, rec.stratum])] <- NA
   
-  # only keep those release strata that appears in statum.index$labels
+# only keep those release strata that appears in statum.index$labels
   recovery <- recovery[ recovery[,rel.stratum] %in% stratum.index$stratum.label,]
-  # only keep those recovery strata that appears in statum.index$labels
+# only keep those recovery strata that appears in statum.index$labels
   recovery <- recovery  [ recovery  [,rec.stratum] %in% stratum.index$stratum.label | is.na(recovery[,rec.stratum]),]
   commercial<-commercial[ commercial[,rec.stratum] %in% stratum.index$stratum.label,]
   
-  # add the stratum index values to original data frame for releases and recoveries
+# add the stratum index values to original data frame for releases and recoveries
   recovery <- merge(recovery, plyr::rename(stratum.index, c("stratum.index"="rel.index")), 
                     by.x=rel.stratum, by.y="stratum.label", all.x=TRUE)
   recovery <- merge(recovery, plyr::rename(stratum.index, c("stratum.index"="rec.index")), 
                     by.x=rec.stratum, by.y="stratum.label", all.x=TRUE)
   
-  # compute statistics
-  
-  # total number of releases by the stratum index
+# compute statistics
+# total number of releases by the stratum index
   n1.df <- plyr::ddply(recovery, "rel.index", plyr::summarize, n1=length(rel.index))
-  # impute any zeroes for no releases in a stratum. We only impute up to larges stratum index
+# impute any zeroes for no releases in a stratum. We only impute up to larges stratum index
   max.rel.index <- max(n1.df$rel.index)
   n1.df <- merge(n1.df,  plyr::rename(stratum.index[,"stratum.index", drop=FALSE], c("stratum.index"="rel.index")), all.y=TRUE)
   n1.df <- n1.df[ n1.df$rel.index <= max.rel.index,]
   n1.df$n1[ is.na(n1.df$n1)] <- 0
   n1.df <- n1.df[ order(n1.df$rel.index),]
-  #browser()
-  # get the cross classification of releases x recoveries. We first need to insert 0's in missing combinations
+
+# get the cross classification of releases x recoveries. We first need to insert 0's in missing combinations
   m2.large <- plyr::ddply(recovery, "rel.index", function(x){
     m2.df <- plyr::ddply(x, "rec.index", plyr::summarize, m2=length(rec.index))
-    # impute any zeroes for no recoveries in a stratum. 
+# impute any zeroes for no recoveries in a stratum. 
     m2.df <- merge(m2.df,  plyr::rename(stratum.index[,"stratum.index", drop=FALSE], c("stratum.index"="rec.index")),
                    all.y=TRUE)
     m2.df$m2[ is.na(m2.df$m2)] <- 0
     m2.df
   })
-  # insert 0 for any release strata with no tags put out
+# insert 0 for any release strata with no tags put out
   m2.all.comb <- expand.grid(rel.index= n1.df$rel.index, rec.index=stratum.index$stratum.index)
   m2.large <- merge(m2.large, m2.all.comb, all.y=TRUE)
   m2.large$m2[ is.na(m2.large$m2)] <- 0
   
-  # now we cross classify
+# now we cross classify
   m2.full <- as.matrix(xtabs(m2~rel.index+rec.index, data=m2.large))
   
-  # rotate each row to the left because you can't recover before releases
-  # we put the amount of (shift+1) in the first column
+# rotate each row to the left because you can't recover before releases
+# we put the amount of (shift+1) in the first column
   m2.red <- plyr::aaply(cbind(n1.df$rel.index,m2.full), 1, function(x){
-    #browser()
+
     rot.vec <- c((x[1]+1):length(x), 2:(x[1]+1))
     x <- x[rot.vec]
     x[-length(x)]
   })
-  #browser()
-  # remove columns at the left that are all zero
+
+# remove columns at the left that are all zero
   all.zero <- apply(m2.red==0, 2, all)
   remove.right <- rev(cumprod(rev(all.zero)))
   m2.red <- m2.red[, !remove.right]
   
-  # remove columns at the left that are all zero
+# remove columns at the left that are all zero
   all.zero <- apply(m2.red==0, 2, all)
   remove.left <- cumprod(all.zero)
   m2.red <- m2.red[, !remove.left]
   
-  # Find the commercial recoveries by rec.stratum
-  # Merge the stratum index
-  # browser()
+# Find the commercial recoveries by rec.stratum
+# Merge the stratum index
+
   commercial <- merge(commercial, plyr::rename(stratum.index, c("stratum.index"="rec.index")), 
                       by.x=rec.stratum, by.y="stratum.label", all.x=TRUE)
-  #browser()
+
   u2.df <- plyr::ddply(commercial, "rec.index", function(x){
     u2=sum(x[,catch.var])
     data.frame(u2=u2)
   })
-  # impute zeros for missing recovery strata
+# impute zeros for missing recovery strata
   u2.df <-merge(u2.df,  plyr::rename(stratum.index[,"stratum.index", drop=FALSE], c("stratum.index"="rec.index")), all.y=TRUE)
   u2.df$u2[ is.na(u2.df$u2)] <- 0
   Qu2.df <- u2.df[ order(u2.df$rec.index),]
-  #browser()
+
   commercial.catch.df <- u2.df 
-  # subtract the recoveries of marked fish
+# subtract the recoveries of marked fish
   u2.df.before.correction <- u2.df$u2 - apply(m2.full,2,sum)
   u2.df$u2 <- pmax(0,u2.df.before.correction)
   
-  #browser()
-  # round everything to integer values
+# round everything to integer values
   n1.df <- round(n1.df)
   m2.full <- round(m2.full)
   m2.red  <- round(m2.red)
@@ -148,8 +144,7 @@ fit.BTSPAS <- function( input.data, prefix="BTSPAS", debug=FALSE, increase.itera
   taku.title  <- paste(taku.prefix," TSPND NP")
   
   
-  #write out a csv file with the releases and recoveries in one giant data frame
-  #browser()
+#write out a csv file with the releases and recoveries in one giant data frame
   n1.df.copy <- input.data$n1.df
   m2.full.copy <- as.data.frame.matrix(input.data$m2.full)
   colnames(m2.full.copy) <- input.data$stratum.index$stratum.label[1:ncol(m2.full.copy)]
@@ -171,28 +166,28 @@ fit.BTSPAS <- function( input.data, prefix="BTSPAS", debug=FALSE, increase.itera
   write.csv(out.df, paste(taku.prefix,"-SummaryStatisticsInMatrixForm.csv",sep=""), row.names=TRUE)
   
   
-  # what is the strata identification number (statistical week from start of year)?
+# what is the strata identification number (statistical week from start of year)?
   taku.sweek <- input.data$stratum.index$stratum.index
   
-  # releases, recoveries, and commercial catch
+# releases, recoveries, and commercial catch
   taku.n1 <- input.data$n1.df$n1
   taku.m2 <- input.data$m2.red  # we want the reduced matrix
   taku.u2 <- input.data$u2.df$u2
   
-  # are there any jumps in the abundance?
+# are there any jumps in the abundance?
   taku.jump.after <- NULL    # list sample times after which jump in number occurs
   
-  # are there any bad values that need to be adjusted?
+# are there any bad values that need to be adjusted?
   taku.bad.n1     <- c()     # list sample times of bad n1 values
   taku.bad.m2     <- c()     # list sample times of bad m2 values
   taku.bad.u2     <- c()     # list sample times of bad u2 values
   
-  # are there any days where the capture probability is fixed in advance?, i.e. because no commercial fishery
-  #  taku.logitP.fixed        <- taku.sweek[which(taku.u2 ==0)]
+# are there any days where the capture probability is fixed in advance?, i.e. because no commercial fishery
+#  taku.logitP.fixed        <- taku.sweek[which(taku.u2 ==0)]
   taku.logitP.fixed        <- taku.sweek[which(total.cap ==0)]
   taku.logitP.fixed.values <- rep(-10, length(taku.logitP.fixed))
-  #browser()
-  ## Run TSPNDE model
+
+## Run TSPNDE model
   taku.fit.tspndenp <- TimeStratPetersenNonDiagErrorNP_fit(
     title=      taku.title,
     prefix=     taku.prefix,
@@ -210,7 +205,7 @@ fit.BTSPAS <- function( input.data, prefix="BTSPAS", debug=FALSE, increase.itera
     n.burnin=ifelse(debug,100,1000), n.sims=ifelse(debug,30,500),
     debug=FALSE
   )
-  # Rename files that were created.
+# Rename files that were created.
   
   file.rename("data.txt",       paste(taku.prefix,".data.txt",sep=""))
   file.rename("CODAindex.txt",  paste(taku.prefix,".CODAindex.txt",sep=""))
@@ -222,11 +217,10 @@ fit.BTSPAS <- function( input.data, prefix="BTSPAS", debug=FALSE, increase.itera
   file.rename("inits3.txt",     paste(taku.prefix,".inits3.txt",sep=""))
   
   
-  #Save the information for later retreival if needed
+# Save the information for later retreival if needed
   save(list=c("taku.fit.tspndenp"), file="taku-fit-tspndenp-saved.Rdata")  # save the results from this run
   setwd("..")# return back
 }
-
 
 # Make the call to BTSPAS allowing for dropout/fallback
 fit.BTSPAS.dropout <- function( input.data, prefix="BTSPAS", debug=FALSE, n, dropout){
@@ -243,8 +237,7 @@ fit.BTSPAS.dropout <- function( input.data, prefix="BTSPAS", debug=FALSE, n, dro
   taku.title  <- paste(taku.prefix," TSPND NP")
   
   
-  #write out a csv file with the releases and recoveries in one giant data frame
-  #browser()
+# write out a csv file with the releases and recoveries in one giant data frame
   n1.df.copy <- input.data$n1.df
   m2.full.copy <- as.data.frame.matrix(input.data$m2.full)
   colnames(m2.full.copy) <- input.data$stratum.index$stratum.label[1:ncol(m2.full.copy)]
@@ -266,27 +259,27 @@ fit.BTSPAS.dropout <- function( input.data, prefix="BTSPAS", debug=FALSE, n, dro
   write.csv(out.df, paste(taku.prefix,"-SummaryStatisticsInMatrixForm.csv",sep=""), row.names=TRUE)
   
   
-  # what is the strata identification number (statistical week from start of year)?
+# what is the strata identification number (statistical week from start of year)?
   taku.sweek <- input.data$stratum.index$stratum.index
   
-  # releases, recoveries, and commercial catch
+# releases, recoveries, and commercial catch
   taku.n1 <- input.data$n1.df$n1
   taku.m2 <- input.data$m2.red  # we want the reduced matrix
   taku.u2 <- input.data$u2.df$u2
   
-  # are there any jumps in the abundance?
+# are there any jumps in the abundance?
   taku.jump.after <- NULL    # list sample times after which jump in number occurs
   
-  # are there any bad values that need to be adjusted?
+# are there any bad values that need to be adjusted?
   taku.bad.n1     <- c()     # list sample times of bad n1 values
   taku.bad.m2     <- c()     # list sample times of bad m2 values
   taku.bad.u2     <- c()     # list sample times of bad u2 values
   
-  # are there any days where the capture probability is fixed in advance?, i.e. because no commercial fishery
-  #taku.logitP.fixed        <- taku.sweek[which(taku.u2 ==0)]
+# are there any days where the capture probability is fixed in advance?, i.e. because no commercial fishery
+#taku.logitP.fixed        <- taku.sweek[which(taku.u2 ==0)]
   taku.logitP.fixed        <- taku.sweek[which(total.cap ==0)]
   taku.logitP.fixed.values <- rep(-10, length(taku.logitP.fixed))
-  #browser()
+
   ## Run TSPNDE model
   taku.fit.tspndenp <- TimeStratPetersenNonDiagErrorNPMarkAvail_fit(
     title=      taku.title,
@@ -303,10 +296,9 @@ fit.BTSPAS.dropout <- function( input.data, prefix="BTSPAS", debug=FALSE, n, dro
     logitP.fixed.values=taku.logitP.fixed.values,
     n.iter=ifelse(debug,1000,30000), n.burnin=ifelse(debug,100,1000), n.sims=ifelse(debug,30,500),
     marked_available_n=n, marked_available_x=n-dropout,
-    debug=FALSE
-  )
-  # Rename files that were created.
+    debug=FALSE)
   
+# Rename files that were created.
   file.rename("data.txt",       paste(taku.prefix,".data.txt",sep=""))
   file.rename("CODAindex.txt",  paste(taku.prefix,".CODAindex.txt",sep=""))
   file.rename("CODAchain1.txt", paste(taku.prefix,".CODAchain1.txt",sep=""))
@@ -315,7 +307,6 @@ fit.BTSPAS.dropout <- function( input.data, prefix="BTSPAS", debug=FALSE, n, dro
   file.rename("inits1.txt",     paste(taku.prefix,".inits1.txt",sep=""))
   file.rename("inits2.txt",     paste(taku.prefix,".inits2.txt",sep=""))
   file.rename("inits3.txt",     paste(taku.prefix,".inits3.txt",sep=""))
-  
   
   # Save the information for later retreival if needed
   save(list=c("taku.fit.tspndenp"), file="taku-fit-tspndenp-saved.Rdata")  # save the results from this run
--- code/inseason_analysis.R
+++ code/inseason_analysis.R
@@ -1,5 +1,5 @@
 # load libraries
-#devtools::install_github("cschwarz-stat-sfu-ca/BTSPAS", dependencies = TRUE, build_vignettes = TRUE)
+#devtools::install_github("cschwarz-stat-sfu-ca/BTSPAS", dependencies = TRUE, build_vignettes = TRUE) #only load once then comment out
 library(BTSPAS) 
 library(ggplot2)
 library(lubridate)
@@ -8,10 +8,9 @@ library(filesstrings)
 source("code/functions.R")
 
 fw.stat.weeks <- 23:28   # stat weeks with releases and recoveries to  be included
-Year<-2018 #input year
+Year<-2018 # input year
 
 # load data and ensure variable names match
-#change data location to sharepoint!!!!
 read.csv('data/2019_inseason/release_data.csv', header=TRUE, as.is=TRUE, strip.white=TRUE) -> release
 release$ReleaseDate <- lubridate::ymd(release$ReleaseDate)
 dim(release)
@@ -32,17 +31,12 @@ catch <- plyr::rename(catch, c("Date"="RecoveryDate",
 catch$RecoveryType="Commercial"
 head(catch)
 
-# some basic statistics
 xtabs(~ReleaseStatWeek,  data=release,exclude=NULL, na.action=na.pass)
 xtabs(~RecoveryStatWeek, data=recap,  exclude=NULL, na.action=na.pass)
 xtabs(CatchWithTags~RecoveryStatWeek, data=catch,  exclude=NULL, na.action=na.pass)
 
-
 # Figure out the half week strata
-# We will divide Sunday -> Wednesday as the first half= Commercial Opening
-#                Thursday -> Saturday as the second half
-#
-# First get all the dates in the study
+# Divide Sunday -> Wednesday as the first half= Commercial Opening and Thursday -> Saturday as the second half
 strata <- rbind( 
   plyr::rename(release, c("ReleaseStatWeek"="StatWeek",
                           "ReleaseDate"="Date"))[,c("Year","Date","StatWeek")],
@@ -62,51 +56,41 @@ strata$HalfStatWeek <- strata$StatWeek + .1 +
   .1*strata$dow %in% c(5,6,7)
 head(strata)
 
-# mergback the strata halfweek back to release, recovery,
-# and commercial catch
-
+# merge back the strata halfweek back to release, recovery, and commercial catch
 dim(release)
 release <- merge(release, 
-                 plyr::rename(strata, 
-                              c("Date"="ReleaseDate",
-                                "HalfStatWeek"="ReleaseHalfStatWeek"))
+                 plyr::rename(strata, c("Date"="ReleaseDate","HalfStatWeek"="ReleaseHalfStatWeek"))
                  [,c("ReleaseDate","ReleaseHalfStatWeek")],
                  all.x=TRUE)
 dim(release)
 
 dim(recap)
 recap <- merge(recap, 
-               plyr::rename(strata, 
-                            c("Date"="RecoveryDate",
-                              "HalfStatWeek"="RecoveryHalfStatWeek"))
+               plyr::rename(strata,  c("Date"="RecoveryDate", "HalfStatWeek"="RecoveryHalfStatWeek"))
                [,c("RecoveryDate","RecoveryHalfStatWeek")],
                all.x=TRUE)
 dim(recap)
 
-# but a recap that occurs in the second half of the week is assumed to have occured
-# in the commerical opening. You need to change the data as needed for the half week
-# analysis and is likely to be year specific
+# but a recap that occurs in the second half of the week is assumed to have occurred
+# in the commerical opening. Change the data as needed for the half week analysis; likely to be year specific
 xtabs(~RecoveryHalfStatWeek, data=recap)
 
 select <- (recap$RecoveryHalfStatWeek %% 1) > .15  # recoveries in second half of week
 sum(select)
 xtabs(~RecoveryHalfStatWeek, data=recap[select,])
-recap$RecoveryHalfStatWeek[select] <- recap$RecoveryHalfStatWeek[select] - .1 # shif back to the opening
+recap$RecoveryHalfStatWeek[select] <- recap$RecoveryHalfStatWeek[select] - .1 # shift back to the opening
 
 xtabs(~RecoveryHalfStatWeek, data=recap)
 
 
 dim(catch)
 catch <- merge(catch, 
-               plyr::rename(strata, 
-                            c("Date"="RecoveryDate",
-                              "HalfStatWeek"="RecoveryHalfStatWeek"))
+               plyr::rename(strata, c("Date"="RecoveryDate","HalfStatWeek"="RecoveryHalfStatWeek"))
                [,c("RecoveryDate","RecoveryHalfStatWeek")],
                all.x=TRUE)
 dim(catch)
 
-# merge the recaptures with the releases
-# check that recapture tag numbers match
+# merge the recaptures with the releases; check that recapture tag numbers match
 setdiff(recap$TagID, release$TagID)
 
 dim(release)
@@ -144,7 +128,6 @@ fw.prefix.dropout <- paste("Taku-FW-Inseason-W",round(min(fw.stat.weeks)),
                            "-W",round(max(fw.stat.weeks)),"-fallback-",sep="")
 fit.BTSPAS.dropout(fw.data,prefix=fw.prefix.dropout, n=50, dropout=12, add.ones.at.start=TRUE)
 
-
 # Half Week BTSPAS analysis
 # Define the stratum variable as 1 = first stat week, 2=second stat week etc
 hw.stat.weeks <- sort(as.vector(outer(fw.stat.weeks, c(.1,.2), "+")))  # releases and recoveries 
@@ -155,7 +138,6 @@ hw.stratum.index <- data.frame(stratum.index=1:length(hw.stat.weeks),
                                stringsAsFactors=FALSE)
 hw.stratum.index
 
-
 # get the data necessary for the call to BTSPAS
 hw.data <- BTSPAS_input(relrecap, catch, "ReleaseHalfStatWeek", "RecoveryHalfStatWeek",
                         hw.stratum.index, catch.var="CatchWithTags")
@@ -164,23 +146,20 @@ hw.data <- BTSPAS_input(relrecap, catch, "ReleaseHalfStatWeek", "RecoveryHalfSta
 hw.prefix <- gsub("FW","HW",fw.prefix)
 fit.BTSPAS(hw.data,prefix=hw.prefix, add.ones.at.start=TRUE)
 
-
-# fit the BTSPAS model with fall back (say n=50, x=12)
+# fit the BTSPAS model with fall back (say n=50, x=11)
 hw.prefix.dropout <- gsub("FW","HW",fw.prefix.dropout)
-fit.BTSPAS.dropout(hw.data,prefix=hw.prefix.dropout, n=50, dropout=12, add.ones.at.start=TRUE)
-
+fit.BTSPAS.dropout(hw.data,prefix=hw.prefix.dropout, n=50, dropout=11, add.ones.at.start=TRUE)
 
 # Make a table of the estimates from the various sets of weeks etc
-##### Extract the outputs
-
-## Extract the results from the various fits
+# Extract the results from the various fits
 file.names <-dir()
+
 # Extract the directories with the fits
 file.names.fits<- file.names[grepl(paste("^Taku-"), file.names)]
 file.names.fits
 
 # make a pdf file of the fitted curves
-pdf(paste("Inseason-all-fits.pdf",sep=""))
+pdf(paste("output/Inseason-all-fits.pdf",sep=""))
 plyr::l_ply(file.names.fits, function(x){
   cat("Extracting final plot from ", x, "\n")
   load(file.path(x, "taku-fit-tspndenp-saved.Rdata"))
@@ -208,8 +187,7 @@ run.size
 write.csv(run.size,
           file=paste("output/inseason-run.size.csv",sep=""))
 
-## Extract the Petersen estimators
-#
+# Extract the Petersen estimators
 # Extract all of the estimates of the total run size
 run.pet.size <- plyr::ldply(file.names.fits, function(x){
   cat("Extracting Petersen from ",x,"\n")
@@ -230,22 +208,23 @@ run.pet.size
 write.csv(run.pet.size,
           file="output/Inseason-PP.run.size.csv")
 
-#taku.prefix <- paste(fw.prefix,"-",Year, sep="")
-#files_old <- paste0(getwd(), "/", taku.prefix)
-#files_new <- paste0(getwd(), "/output/", taku.prefix)
-#file.move(from = files_old, to = files_new)
+#move files to correct directory
+taku.prefix <- paste(fw.prefix,"-",Year, sep="")
+files_old <- paste0(getwd(), "/", taku.prefix)
+files_new <- paste0(getwd(), "/output/", taku.prefix)
+file.move(from = files_old, to = files_new)
 
-#taku.prefix <- paste(fw.prefix.dropout,"-",Year, sep="")
-#files_old <- paste0(getwd(), "/", taku.prefix)
-#files_new <- paste0(getwd(), "/output/", taku.prefix)
-#file.move(from = files_old, to = files_new)
+taku.prefix <- paste(fw.prefix.dropout,"-",Year, sep="")
+files_old <- paste0(getwd(), "/", taku.prefix)
+files_new <- paste0(getwd(), "/output/", taku.prefix)
+file.move(from = files_old, to = files_new)
 
-#taku.prefix <- paste(hw.prefix,"-",Year, sep="")
-#files_old <- paste0(getwd(), "/", taku.prefix)
-#files_new <- paste0(getwd(), "/output/", taku.prefix)
-#file.move(from = files_old, to = files_new)
+taku.prefix <- paste(hw.prefix,"-",Year, sep="")
+files_old <- paste0(getwd(), "/", taku.prefix)
+files_new <- paste0(getwd(), "/output/", taku.prefix)
+file.move(from = files_old, to = files_new)
 
-#taku.prefix <- paste(hw.prefix.dropout,"-",Year, sep="")
-#files_old <- paste0(getwd(), "/", taku.prefix)
-#files_new <- paste0(getwd(), "/output/", taku.prefix)
-#file.move(from = files_old, to = files_new)
+taku.prefix <- paste(hw.prefix.dropout,"-",Year, sep="")
+files_old <- paste0(getwd(), "/", taku.prefix)
+files_new <- paste0(getwd(), "/output/", taku.prefix)
+file.move(from = files_old, to = files_new)
