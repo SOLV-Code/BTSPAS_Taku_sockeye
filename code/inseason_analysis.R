8908d9675fe13900a3c8eeb5186754fc87c7d285
Miller <sara.miller@dfg.alaska.local>
2019-04-25 10:27:27 -0800

    update dropout rate

--- code/inseason_analysis.R
+++ code/inseason_analysis.R
@@ -126,7 +126,7 @@ fit.BTSPAS(fw.data,prefix=fw.prefix, add.ones.at.start=FALSE)
 # fit the BTSPAS model with fall back (say n=50, x=11)
 fw.prefix.dropout <- paste("Taku-FW-Inseason-W",round(min(fw.stat.weeks)),
                            "-W",round(max(fw.stat.weeks)),"-fallback-",sep="")
-fit.BTSPAS.dropout(fw.data,prefix=fw.prefix.dropout, n=50, dropout=12, add.ones.at.start=FALSE)
+fit.BTSPAS.dropout(fw.data,prefix=fw.prefix.dropout, n=50, dropout=11, add.ones.at.start=FALSE)
 
 # Half Week BTSPAS analysis
 # Define the stratum variable as 1 = first stat week, 2=second stat week etc
